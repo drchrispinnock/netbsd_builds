61df0a73846f549e8ddfae6a68a15549592c0bb1
Chris Pinnock <chris_pinnock@mac.com>
2021-05-12 10:41:09 +0100

    and build it

--- aws/setup_netbsd.sh
+++ aws/setup_netbsd.sh
@@ -6,6 +6,9 @@
 cdn='http://nycdn.netbsd.org/pub/NetBSD-daily/HEAD/latest/source/sets/'
 sourcefiles="src.tgz syssrc.tgz sharesrc.tgz gnusrc.tgz xsrc.tgz"
 
+machine="amd64"
+jobs=8
+
 if [ "$1" = "curl" ]; then
 	curl=1
 fi
@@ -27,8 +30,24 @@ echo "===> Extracting sources..." >&2
 sleep 1
 
 for i in $sourcefiles; do
+	echo "$i" >&2
 	tar zxf sources/$i
+	[ "$?" != "0" ] && echo "Error extracting $i" >&2 && exit 1
 done
 
-mv usr netbsd-current
+cd usr/src
 
+echo "===> Building test $machine build" >&2
+sleep 1
+
+./build.sh -j $jobs -U -m $machine build > ../../build-log 2>&1
+[ "$?" != "0" ] && echo "Error building!" >&2 && exit 1
+
+echo "===> Building test $machine release" >&2
+sleep 1
+
+./build.sh -j $jobs -U -m $machine release > ../../release-log 2>&1
+[ "$?" != "0" ] && echo "Error building release!" >&2 && exit 1
+
+# Need to preserve the logs somewhere
+#
