0a6e1fbf2865f3778bfa1037ebb494ef95953821
Chris Pinnock <chris_pinnock@mac.com>
2021-05-24 13:23:45 +0000

    add mac 64

--- buildwrapper.sh
+++ buildwrapper.sh
@@ -24,7 +24,7 @@ supported="amd64 i386 sparc64 evbppc hpcarm evbarm64-el evbarm64-eb evbmips64-eb
 
 # Tier 2/Organic platforms
 #
-organic="acorn32 algor alpha amiga amigappc arc atari bebox cats cesfic	cobalt dreamcast epoc32 emips evbsh3-eb evbsh3-el ews4800mips hp300 hppa hpcmips hpcsh ibmnws iyonix landisk luna68k mac68k macppc mipsco mmeye mvme68k mvmeppc netwinder news68k newsmips next68k ofppc pmax prep rs6000 sandpoint sbmips-eb sbmips-el sbmips64-eb sbmips64-el sgimips shark sparc sun2 sun3 vax x68k zaurus"
+organic="acorn32 algor alpha amiga amigappc arc atari bebox cats cesfic	cobalt dreamcast epoc32 emips evbsh3-eb evbsh3-el ews4800mips hp300 hppa hpcmips hpcsh ibmnws iyonix landisk luna68k mac68k macppc macppc64 mipsco mmeye mvme68k mvmeppc netwinder news68k newsmips next68k ofppc pmax prep rs6000 sandpoint sbmips-eb sbmips-el sbmips64-eb sbmips64-el sgimips shark sparc sun2 sun3 vax x68k zaurus"
 other="ia64 riscv"
 
 # All targets
