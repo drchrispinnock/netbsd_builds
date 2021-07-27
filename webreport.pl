a3c2084f67f299d0f013e15881280269e35dec8a
Chris Pinnock <chris_pinnock@mac.com>
2021-07-27 19:48:01 +0000

    last updated date

--- webreport.pl
+++ webreport.pl
@@ -191,6 +191,9 @@ foreach my $platform (@Platforms) {
 }
 
 print OUT "</table>";
+
+my $dispdate = `date`;
+print OUT "<center><em>Last updated $dispdate</em></center>\n";
 print OUT "</body></html>\n";
 close OUT;
 system("mv $webresultsroot/index.html.new $webresultsroot/index.html");
