9be1d3e3361c27061d08d3fb8e07da523c77f1fe
Chris Pinnock <chris_pinnock@mac.com>
2022-01-02 10:23:23 +0000

    tiers

--- webreport.pl
+++ webreport.pl
@@ -28,6 +28,7 @@ my %Platforms;
 
 my $sort_by_results = 1;
 my $usetiers = 1; # 0 and it sorts alphabetically
+$usetiers = 0; # 0 and it sorts alphabetically
 my @tier1 = qw(amd64 i386 sparc64 evbppc hpcarm evbmips64-eb evbmips64-el evbmips-eb evbmips-el evbarmv4-el evbarmv4-eb evbarmv5-el evbarmv5hf-el evbarmv5-eb evbarmv5hf-eb evbarmv6-el evbarmv6hf-el evbarmv6-eb evbarmv6hf-eb evbarmv7-el evbarmv7-eb evbarmv7hf-el evbarmv7hf-eb evbarm64-el evbarm64-eb);
 
 my @tier2 = qw(acorn32 algor alpha amiga amigappc arc atari bebox cats cesfic cobalt dreamcast epoc32 emips evbsh3-eb evbsh3-el ews4800mips hp300 hppa hpcmips hpcsh ibmnws iyonix landisk luna68k mac68k macppc mipsco mmeye mvme68k mvmeppc netwinder news68k newsmips next68k ofppc pmax prep rs6000 sandpoint sbmips-eb sbmips-el sbmips64-eb sbmips64-el sgimips shark sparc sun2 sun3 vax x68k zaurus);
